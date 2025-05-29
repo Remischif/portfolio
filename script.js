63a5e1b6c28a618f1572e1755682ffd4ea879aa8
Remi <remi.schifferling@hotmail.com>
2025-05-30 01:22:15 +0200

    icons and footer update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const dateElement = document.getElementById("datetime");
 
 function updateDateTime() {
@@ -8,14 +7,3 @@ function updateDateTime() {
 
 updateDateTime(); // première exécution
 setInterval(updateDateTime, 1000); // mise à jour toutes les secondes (1000 ms = 1 s)
-=======
-const dateElement = document.getElementById("datetime");
-
-function updateDateTime() {
-  const now = new Date();
-  dateElement.textContent = `Nous sommes le ${now.toLocaleDateString()} à ${now.toLocaleTimeString()}`;
-}
-
-updateDateTime(); // première exécution
-setInterval(updateDateTime, 1000); // mise à jour toutes les secondes (1000 ms = 1 s)
->>>>>>> b402314719fc348cd0786e9c0c811a82ea400005
